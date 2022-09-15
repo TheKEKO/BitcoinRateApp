aa237d2162f6f5d6d9864543161ebfb06ee720b1
Aleksandr F <a7214370@gmail.com>
2022-09-15 11:58:23 +0300

    Setup project;

--- BitcoinRateApp/Models/CoinManager.swift
+++ BitcoinRateApp/Models/CoinManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=10CA60A2-A56B-4F23-94BF-51B4AD7AD5C5
+//https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=10CA60A2-A56B-4F23-94BF-51B4AD7AD5C5
 
 import Foundation
 
