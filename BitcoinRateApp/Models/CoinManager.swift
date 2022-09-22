9337ea32251936826cf3ce06e7990d0b6d4abed8
Aleksandr F <a7214370@gmail.com>
2022-09-22 13:46:07 +0300

    The app is complete.

--- BitcoinRateApp/Models/CoinManager.swift
+++ BitcoinRateApp/Models/CoinManager.swift
@@ -21,7 +21,7 @@ struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "10CA60A2-A56B-4F23-94BF-51B4AD7AD5C5"
-
+    
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     func getCoinPrice(for currency: String) {
