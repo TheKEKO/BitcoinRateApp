1643ed7b187e5d8b6913543351ba903d009c5b06
Aleksandr F <a7214370@gmail.com>
2022-09-22 13:45:33 +0300

    The app is complete.

--- BitcoinRateApp/View/ViewController.swift
+++ BitcoinRateApp/View/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var currencyPicker: UIPickerView!
     
     var coinManager = CoinManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,19 +46,19 @@ extension ViewController: CoinManagerDelegate {
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-          return 1
-      }
-      
-      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-          return coinManager.currencyArray.count
-      }
-      
-      func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-          return coinManager.currencyArray[row]
-      }
-      
-      func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-          let selectedCurrency = coinManager.currencyArray[row]
-          coinManager.getCoinPrice(for: selectedCurrency)
-      }
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
 }
